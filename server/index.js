413f8cb058c27fba47356f4565031c6135d93fa3
yanxugong <745710524@qq.com>
2020-02-12 18:04:11 +0800

    add controller

--- server/index.js
+++ server/index.js
@@ -3,15 +3,13 @@
  * @Author: yanxu gong
  * @Date: 2020-01-30 12:34:28
  * @LastEditors  : yanxu gong
- * @LastEditTime : 2020-01-30 18:39:03
+ * @LastEditTime : 2020-02-12 18:02:08
  */
+const Controller = require('./controller')
 const http = require('http')
-const path = require('path')
-const fse = require('fs-extra')
-const multiparty = require('multiparty')
-
 const server = http.createServer()
-const UPLOAD_DIR = path.resolve(__dirname, '..', 'target') // 大文件存储目录
+
+const controller = new Controller()
 
 server.on('request', async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
@@ -22,28 +20,19 @@ server.on('request', async (req, res) => {
     return
   }
 
-  const form = new multiparty.Form()
+  if (req.url === '/verify') {
+    await controller.handleVerifyUpload(req, res)
+    return
+  }
 
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      return
-    }
-    const [chunk] = fields.chunk
-    const [hash] = fields.hash
-    const [filename] = fields.filename
-    const chunkDir = path.resolve(UPLOAD_DIR, filename)
+  if (req.url === '/merge') {
+    await controller.handleMerge(req, res)
+    return
+  }
 
-    // 切片目录不存在，创建切片目录
-    if (!fse.existsSync(chunkDir)) {
-      await fse.mkdirs(chunkDir)
-    }
-
-    // fs-extra 专用方法，类似 fs.rename 并且跨平台
-    // fs-extra 的 rename 方法 windows 平台会有权限问题
-    // https://github.com/meteor/meteor/issues/7852#issuecomment-255767835
-    await fse.move(chunk.path, `${chunkDir}/${hash}`)
-    res.end('received file chunk')
-  })
+  if (req.url === '/') {
+    await controller.handleFormData(req, res)
+  }
 })
 
-server.listen(3000, () => console.log('listening 3000...'))
+server.listen(3000, () => console.log('正在监听 3000 端口'))
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -3,12 +3,14 @@
  * @Author: yanxu gong
  * @Date: 2020-01-26 20:52:05
  * @LastEditors  : yanxu gong
- * @LastEditTime : 2020-01-30 18:53:00
+ * @LastEditTime : 2020-02-12 17:23:59
  -->
 <template>
   <div class="upload">
     <input type="file" @change="handleFileChange" />
     <el-button @click="handleUpload">上传</el-button>
+    <el-button @click="handlePause" v-if="isPaused">暂停</el-button>
+    <el-button @click="handleResume" v-else>恢复</el-button>
   </div>
 </template>
 
@@ -20,8 +22,26 @@ export default {
     container: {
       file: null
     },
-    data: []
+    data: [],
+    isPaused: false,
+    fakeUploadPercentage: 0
   }),
+  watch: {
+    uploadPercentage(newValue) {
+      if (newValue > this.fakeUploadPercentage) {
+        this.fakeUploadPercentage = newValue
+      }
+    }
+  },
+  computed: {
+    uploadPercentage() {
+      if (!this.container.file || !this.data.length) return 0
+      const loaded = this.data
+        .map(item => item.size * item.percentage)
+        .reduce((acc, cur) => acc + cur)
+      return parseInt((loaded / this.container.file.size).toFixed(2))
+    }
+  },
   methods: {
     handleFileChange(e) {
       const [file] = e.target.files
@@ -41,53 +61,129 @@ export default {
       }
       return fileChunkList
     },
-    request({ url, method = 'post', data, headers = {}, requestList }) {
+    request({
+      url,
+      method = 'post',
+      data,
+      headers = {},
+      onProgress = e => e,
+      requestList
+    }) {
       return new Promise(resolve => {
         const xhr = new XMLHttpRequest()
+        xhr.upload.onprogress = onProgress
         xhr.open(method, url)
         Object.keys(headers).forEach(key =>
           xhr.setRequestHeader(key, headers[key])
         )
         xhr.send(data)
         xhr.onload = e => {
+          // 将请求成功的 xhr 从列表中删除
+          if (requestList) {
+            const xhrIndex = requestList.findIndex(item => item === xhr)
+            requestList.splice(xhrIndex, 1)
+          }
           resolve({
             data: e.target.response
           })
         }
+        requestList && requestList.push(xhr)
       })
     },
+    createProgressHandler(item) {
+      return e => {
+        item.percentage = parseInt(String((e.loaded / e.total) * 100))
+      }
+    },
+    // 生成文件 hash (web-worker)
+    calculateHash(fileChunkList) {
+      return new Promise(resolve => {
+        // 添加 worker 属性
+        this.container.worker = new Worker('/hash.js')
+        this.container.worker.postMessage({ fileChunkList })
+        this.container.worker.onmessage = e => {
+          const { percentage, hash } = e.data
+          this.hashPercentage = percentage
+          if (hash) {
+            resolve(hash)
+          }
+        }
+      })
+    },
+    handlePause() {
+      this.requestList.forEach(xhr => xhr?.abort())
+      this.requestList = []
+    },
+    async verifyUpload(filename, fileHash) {
+      const { data } = await this.request({
+        url: 'http://localhost:3000/verify',
+        headers: { 'content-type': 'application/json' },
+        data: JSON.stringify({
+          filename,
+          fileHash
+        })
+      })
+      return JSON.parse(data)
+    },
     // 上传切片
-    async uploadChunks() {
+    async uploadChunks(uploadedList = []) {
       const requestList = this.data
-        .map(({ chunk, hash }) => {
+        .filter(({ hash }) => !uploadedList.includes(hash))
+        .map(({ chunk, hash, index }) => {
           const formData = new FormData()
           formData.append('chunk', chunk)
           formData.append('hash', hash)
           formData.append('filename', this.container.file.name)
-          return { formData }
+          return { formData, index }
         })
-        .map(async ({ formData }) =>
-          this.request({ url: 'http://localhost:3000', data: formData })
+        .map(async ({ formData, index }) =>
+          this.request({
+            url: 'http://localhost:3000',
+            data: formData,
+            onProgress: this.createProgressHandler(this.data[index])
+          })
         )
       await Promise.all(requestList) // 并发切片
-      await this.mergeRequest()
+      // 之前上传的切片数量 + 本次上传的切片数量 = 所有切片数量时
+      // 合并切片
+      if (uploadedList.length + requestList.length === this.data.length) {
+        await this.mergeRequest()
+      }
     },
     async mergeRequest() {
       await this.request({
         url: 'http://localhost:3000/merge',
         headers: { 'content-type': 'application/json' },
-        data: JSON.stringify({ filename: this.container.file.name })
+        data: JSON.stringify({ size: SIZE, filename: this.container.file.name })
       })
+    },
+    async handleResume() {
+      const { uploadedList } = await this.verifyUpload(
+        this.container.file.name,
+        this.container.hash
+      )
+      await this.uploadChunks(uploadedList)
     },
     async handleUpload() {
       if (!this.container.file) return
       const fileChunkList = this.createFileChunk(this.container.file)
-      debugger
+      this.container.hash = await this.calculateHash(fileChunkList)
+      const { shouldUpload, uploadedList } = await this.verifyUpload(
+        this.container.file.name,
+        this.container.hash
+      )
+      if (!shouldUpload) {
+        this.$message.success('秒传：上传成功')
+        return
+      }
       this.data = fileChunkList.map(({ file }, index) => ({
+        fileHash: this.container.hash,
         chunk: file,
-        hash: `${this.container.file.name}-${index}` // 文件名 + 数组下标
+        index,
+        hash: `${this.container.file.name}-${index}`, // 文件名 + 数组下标
+        percentage: uploadedList.includes(index) ? 100 : 0
       }))
-      await this.uploadChunks()
+      await this.uploadChunks(uploadedList)
     }
   }
 }
