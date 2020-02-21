18beb4d32921d28bf2ce614f8601bc5ad6cc4165
yanxugong <745710524@qq.com>
2020-02-21 14:47:25 +0800

    new

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Descripttion: 
+ * @Author: yanxu gong
+ * @Date: 2020-02-21 11:26:15
+ * @LastEditors: yanxu gong
+ * @LastEditTime: 2020-02-21 14:42:06
+ -->
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
